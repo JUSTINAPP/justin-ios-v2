bbc53cc1ea60a9f3f02d6096d0c2776afe9095fe
Jonas Allen <jonas@dorja.com>
2026-06-15 20:22:17 +1000

    Initial Commit

---
+++ Justin/JustinApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustinApp.swift
+//  Justin
+//
+//  Created by Jonas Allen on 15/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct JustinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
